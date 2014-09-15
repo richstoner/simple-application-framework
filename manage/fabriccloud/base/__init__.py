03a2677ea4feef9cf723f9fce71639f730d99221
richstoner <stonerri@gmail.com>
2014-09-15 16:30:35 -0400

    fix depends

--- manage/fabriccloud/base/__init__.py
+++ manage/fabriccloud/base/__init__.py
@@ -305,7 +305,7 @@ def installGirderDepends():
     sudo('add-apt-repository ppa:chris-lea/node.js')
     sudo('apt-get update')
     sudo('apt-get install nodejs')
-    sudo('npm install -g grunt grunt-cli')
+    # sudo('npm install -g grunt grunt-cli')
 
     with cd('%s/girder' % (app_path)):
         run('npm install')
